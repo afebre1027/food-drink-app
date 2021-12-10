c3637d23ab1d32e6f7d841bc2d666474c45f0ed2
Dasan Joell <dasanjoell@Dasans-Air.attlocal.net>
2021-12-10 02:50:29 -0500

    added search drinks to html, updated js src, updated fonts on css

--- javascript.js
+++ javascript.js
@@ -1,47 +1,56 @@
 // global variables
-const drinkIngredients = $('.drinkIngredients');
-const drinkDescription = $('#drinkDescription');
-const drinkTitle = $('#drinkTitle');
+const output = $("#output");
 const drinkGroup = $('.drinkGroup');
 const drinkImage = $('#drinkImage');
+let localstorageStringArr = []; //add arr for localstorage search quries - D
 
-drinkGroup.children().on('click', function () {
-  for (let i = 0; i < drinkGroup.children().length; i++) {
+drinkGroup.children().on('click', function (e) {
+  e.preventDefault();
+
+   for (let i = 0; i < drinkGroup.children().length; i++) {
     if (drinkGroup.children([i]).hasClass('pure-button-primary')) {
       drinkGroup.children([i]).removeClass('pure-button-primary');
     }
   }
+  // if button isn't submit - D
+  if($(this).attr("type") !== "submit"){
+    return;
+  }
+  
   $(this).addClass('pure-button-primary');
   getDrink();
 });
 
 const getDrink = async function () {
-  let innerText = $(drinkGroup).find('button.pure-button-primary').attr('id');
+  let innerText = $(drinkGroup).find('#search-drink').val();
+  
+  //if query is less than 1 - D
+  if (innerText.length <= 1){
+    modalPop();
+    return;
+  }
 
-  // this api call only gets title,id and image
+  // this api call only gets title,id and image 
   let response = await fetch(
     `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${innerText}`
-  );
+  )
 
   let data = await response.json();
-
+  
   // chooseing one random drink from the list
-
   let drink = data.drinks[Math.floor(Math.random() * data.drinks.length)];
 
   // desctructuring the object so i can rename
-
   const { strDrink: title, strDrinkThumb: image, idDrink } = drink;
 
   let secondResponse = await fetch(
     `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`
-  );
+  )
+
   let secondData = await secondResponse.json();
-
   const { strInstructions: instruct } = secondData.drinks[0];
-
+  
   // they define the ingredients weird so i have to do this to get what i need
-
   let contents = [];
 
   for (let i = 1; i <= 15; i++) {
@@ -53,26 +62,57 @@ const getDrink = async function () {
     if (ingredient && measurement) {
       contents.push(`${measurement} ${ingredient}`);
     }
-    // if the measr dont exist but ingre push ingre
+    // if the measurment does not exist but ingredients exist push ingredients
     // example salt to taste
     if (!measurement && ingredient) {
       contents.push(ingredient);
     }
   }
-
-  postDrink(title, image, instruct, contents);
+  postDrink(title, image, instruct, contents, innerText); // add inner text for local storage - D
 };
 
-const postDrink = function (title, image, instruct, contents) {
-  drinkTitle.html(title);
-  drinkImage.attr('src', image);
-  drinkDescription.html(instruct);
-  //  gets ride of previous contents
-  drinkIngredients.empty();
+const postDrink = function (title, image, instruct, contents, searchQuery) {
+  let drinkDiv = $('<div class="drink-card">'); //Created a div - D
+  let drinkImage = $('<img width="200px">');  //created a img - D
+  let drinkUl = $('<ul>');    //created a Ul - D
+  let drinkp = $('<p>'); //created a p - D
+
+  drinkDiv.empty(); // empty div for new content -D
+  
+  //Title -D
+  drinkDiv.append(`<h1>${title}</h1>`); 
+ 
+  //Image -D
+  drinkImage.attr('src', image); 
+  drinkDiv.append(drinkImage);       
+  
+  //Instructions -D
+  drinkp.append(instruct);
+  drinkDiv.append(drinkp);
+
+  //Ingredients
   for (let i = 0; i < contents.length; i++) {
     //   https://www.tutorialrepublic.com/faq/how-to-add-li-in-an-existing-ul-using-jquery.php
     // this added lis as children to the existing ul
-    drinkIngredients.append(`<li>${contents[i]}</li>`);
+    drinkUl.append(`<li>${contents[i]}</li>`);
+  }
+  
+  drinkDiv.append(drinkUl);
+  output.html(drinkDiv);
+
+  //Add search query to local storage - D
+  var Items = localStorage.getItem("food-drink-history");
+
+  if(!Items){
+    localstorageStringArr.push(searchQuery);
+    localStorage.setItem("food-drink-history", JSON.stringify(localstorageStringArr));
+  }else{
+    localstorageStringArr.push(searchQuery);
+    console.log(localstorageStringArr);
+    localStorage.setItem("food-drink-history", JSON.stringify(localstorageStringArr));
   }
 };
 
+function modalPop(errorMsg){
+  console.log("Modal Pop up")
+}
